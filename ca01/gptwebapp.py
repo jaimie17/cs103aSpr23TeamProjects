a81777fbbd813ab7a8f12f7691f032572258e0b7
Samiya Islam <76130563+jojo142@users.noreply.github.com>
2023-03-18 16:42:28 -0400

    Update gptwebapp.py

--- ca01/gptwebapp.py
+++ ca01/gptwebapp.py
@@ -18,7 +18,7 @@ On Windows:
 % $env:APIKEY="....." # in powershell
 % python gptwebapp.py
 '''
-from flask import request,redirect,url_for,Flask
+from flask import request, redirect, url_for, Flask
 from gpt import GPT
 import os
 
@@ -27,6 +27,7 @@ gptAPI = GPT(os.environ.get('APIKEY'))
 
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'_5#y2L"F4Q789789uioujkkljkl...8z\n\xec]/'
+
 
 @app.route('/')
 def home():
@@ -37,79 +38,144 @@ def home():
         <a href="{url_for('gptdemo')}">Ask questions to GPT</a>
         <br>
         <a href="{url_for('team_pages')}">Index</a>
-        <a href="{url_for('about')}">Index</a>
     '''
+
+
 @app.route('/index')
 def team_pages():
     ''' display a link to index with team page links'''
     print('processing / route')
     return f'''
         <h1>Team Pages</h1>
-        <a href="{url_for('grammar')}">Grammar Editor</a>
+        <a href="{url_for('grammar')}">Grammar Editor</a> #Jaimie's page
+        #Samiya's page
+        <a href="{url_for('summarization')}"> Summarize Editor</a>
+        <a href="{url_for('synonym')}"> Synonym Editor</a>  #Samiya's page
+        #Gianna's page
+        <a href="{url_for('translation')}"> Translation Editor</a>
+        <a href="{url_for('paraphrase')}"> Paraphase Editor</a>  #Cindy's page
+        <a href="{url_for('poem')}"> Poem Editor</a>  #Allison's page
     '''
+
+
 @app.route('/about')
 def about_page():
     '''display a link to the about page'''
     print('processing / route')
     return f'''
         <h1>About Page</h>
-        
-    
+
 
 @app.route('/gptdemo', methods=['GET', 'POST'])
 def gptdemo():
-    ''' handle a get request by sending a form 
+    ''' handle a get request by sending a form
         and a post request by returning the GPT response
     '''
     if request.method == 'POST':
         prompt = request.form['prompt']
         answer = gptAPI.getResponse(prompt)
         return f'''
-        <h1>GPT Demo</h1>
-        <pre style="bgcolor:yellow">{prompt}</pre>
-        <hr>
+        <h1 > GPT Demo < /h1 >
+        <pre style = "bgcolor:yellow" > {prompt} < /pre >
+        <hr >
         Here is the answer in text mode:
-        <div style="border:thin solid black">{answer}</div>
+        <div style = "border:thin solid black" > {answer} < /div >
         Here is the answer in "pre" mode:
-        <pre style="border:thin solid black">{answer}</pre>
-        <a href={url_for('gptdemo')}> make another query</a>
+        <pre style = "border:thin solid black" > {answer} < /pre >
+        <a href = {url_for('gptdemo')} > make another query < /a >
         '''
     else:
         return '''
-        <h1>GPT Demo App</h1>
+        <h1 > GPT Demo App < /h1 >
         Enter your query below
-        <form method="post">
-            <textarea name="prompt"></textarea>
-            <p><input type=submit value="get response">
-        </form>
+        <form method = "post" >
+            <textarea name = "prompt" > </textarea >
+            <p > <input type = submit value = "get response" >
+        </form >
         '''
 @app.route('/index/grammar', methods=['GET', 'POST'])
 def grammar():
-    ''' handle a get request by sending a form 
+    ''' handle a get request by sending a form
         and a post request by returning the GPT edited text
     '''
     if request.method == 'POST':
         prompt = request.form['prompt']
         answer = gptAPI.edit_grammar(prompt)
         return f'''
-        <h1>GPT Grammar Editor</h1>
+    <h1 > GPT Grammar Editor < /h1 >
         Your input was:
-        <pre style="border:thin solid black">{prompt}</pre>
-        <hr>
-        Your edited text is:
-        <div style="border:thin solid black">{answer}</div>
-        <br>
-        <a href={url_for('grammar')}> edit more text</a>
+        <pre style = "border:thin solid black" > {prompt} < /pre >
+        <hr >
+        Your edited text is :
+        <div style = "border:thin solid black" > {answer} < /div >
+        <br >
+        <a href = {url_for('grammar')} > edit more text < /a >
         '''
     else:
         return '''
-        <h1>GPT Grammar Editor</h1>
+        <h1 > GPT Grammar Editor < /h1 >
         Enter your text below:
-        <form method="post">
-            <textarea name="prompt"></textarea>
-            <p><input type=submit value="get response">
-        </form>
+        <form method = "post" >
+            <textarea name = "prompt" > </textarea >
+            <p > <input type = submit value = "get response" >
+        </form >
         '''
+@app.route('/index/summarization', methods=['GET', 'POST'])
+def summarization():
+    ''' handle a get request by sending a form
+        and a post request by returning the GPT edited text
+    '''
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        answer = gptAPI.edit_grammar(prompt)
+        return f'''
+    <h1 > GPT Summarize Editor < /h1 >
+        Your input was:
+        <pre style = "border:thin solid black" > {prompt} < /pre >
+        <hr >
+        Your edited text is:
+        <div style = "border:thin solid black" > {answer} < /div >
+        <br >
+        <a href = {url_for('grammar')} > edit more text < /a >
+        '''
+    else:
+        return '''
+        <h1 > GPT Summarize Editor < /h1 >
+        Enter your text below:
+        <form method = "post" >
+            <textarea name = "prompt" > </textarea >
+            <p > <input type = submit value = "get response" >
+        </form >
+        '''
+@app.route('/index/synonym', methods=['GET', 'POST'])
+def synonym():
+    ''' handle a get request by sending a form
+        and a post request by returning the GPT edited text
+    '''
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        answer = gptAPI.edit_grammar(prompt)
+        return f'''
+    <h1 > GPT Synonym Editor < /h1 >
+        Your input was:
+        <pre style = "border:thin solid black" > {prompt} < /pre >
+        <hr >
+        Your edited text is:
+        <div style = "border:thin solid black" > {answer} < /div >
+        <br >
+        <a href = {url_for('grammar')} > edit more text < /a >
+        '''
+    else:
+        return '''
+        <h1 > GPT Synonym Editor < /h1 >
+        Enter your text below:
+        <form method = "post" >
+            <textarea name = "prompt" > </textarea >
+            <p > <input type = submit value = "get response" >
+        </form >
+        '''
+
+
 
 if __name__=='__main__':
     # run the code on port 5001, MacOS uses port 5000 for its own service :(

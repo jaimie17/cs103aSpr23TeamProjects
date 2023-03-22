aff6af9f090a2d03c24f1b7ac9839815838a4eba
jaimie17 <jaimielouie@brandeis.edu>
2023-03-22 13:06:34 -0400

    added todolist example code and comments

---
+++ pa03/test_transaction.py
@@ -0,0 +1,6 @@
+'''
+Testing with pytest -- 
+create a file, test_transaction.py, and addtests to it for each method in the Transaction class. 
+It is a good idea to add a test each time you implement a feature. 
+You are testing the Transaction class, not the tracker.py user interface code.
+'''

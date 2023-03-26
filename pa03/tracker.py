9c6f2b044c4701661684a885e957107699443242
jaimie17 <jaimielouie@brandeis.edu>
2023-03-26 00:21:10 -0400

    edited pylint tracker

--- pa03/tracker.py
+++ pa03/tracker.py
@@ -1,5 +1,6 @@
 '''
-tracker.py offers the user the following options and makes calls to the Transaction class to update the database.
+tracker.py offers the user the following options 
+and makes calls to the Transaction class to update the database.
 
 0. quit
 1. show categories
@@ -19,15 +20,16 @@ tracker.py offers the user the following options and makes calls to the Transact
 from transaction import Transaction
 
 class Tracker:
+    '''This class calls the Transaction class to update the database.'''
     def __init__(self, db_file):
-        ''' Written by Samiya'''
-        ''' Initializes the Tracker object and takes in the database file. '''
+        ''' Written by Samiya 
+        Initializes the Tracker object and takes in the database file. '''
         self.db_file = db_file
         self.transactions = Transaction(db_file)
-       
+
     def run(self):
-        ''' Written by Samiya'''
-        ''' Takes in user input of a number to run a finances command. '''
+        ''' Written by Samiya
+        Takes in user input of a number to run a finances command. '''
         print("Welcome to your finance tracker!")
         self.print_menu()
         while True:
@@ -35,7 +37,7 @@ class Tracker:
             if choice == "0":
                 print("Goodbye!")
                 break
-            elif choice == "1":
+            if choice == "1":
                 self.show_categories()
             elif choice == "2":
                 self.add_category()
@@ -59,10 +61,10 @@ class Tracker:
                 self.print_menu()
             else:
                 print("Invalid choice, please try again.")
-                
+
     def print_menu(self):
-        ''' Written by Samiya'''
-        ''' prints out the menu to perform various finance commands. '''
+        ''' Written by Samiya
+        prints out the menu to perform various finance commands. '''
         print("\nPlease choose from the following options:")
         print("0. Quit")
         print("1. Show categories")
@@ -76,10 +78,10 @@ class Tracker:
         print("9. Summarize transactions by year")
         print("10. Summarize transactions by category")
         print("11. Print this menu")
-       
+
     def show_categories(self):
-        ''' Written by Jaimie'''
-        ''' shows the existing categories that the user had inputted. '''
+        ''' Written by Jaimie
+        shows the existing categories that the user had inputted. '''
         categories = self.transactions.get_categories()
         if not categories:
             print("No categories found.")
@@ -87,29 +89,29 @@ class Tracker:
             print("Categories:")
             for category in categories:
                 print(category)
-     
+
     def add_category(self):
-        ''' Written by Jaimie'''
-        ''' adds category to the database '''
+        ''' Written by Jaimie
+        adds category to the database '''
         category = input("Enter new category name: ")
         if self.transactions.add_category(category):
             print(f"Added category {category}.")
         else:
             print(f"Failed to add category {category}.")
-    
+
     def get_category(self):
-        ''' Written by Cindy'''
-        ''' returns the category the user inputs in if it exists. '''
+        ''' Written by Cindy
+        returns the category the user inputs in if it exists. '''
         category = input("Enter category name: ")
         if self.transactions.get_category(category):
             print(f"Found category {category}.")
         else:
             print(f"Failed to find category {category}.")
-    
-    
+
+
     def modify_category(self):
-        ''' Written by Cindy'''
-        ''' modifies the name of an existing category name. '''
+        ''' Written by Cindy
+        modifies the name of an existing category name. '''
         category = input("Enter category to modify: ")
         new_category = input("Enter new category name: ")
         if self.transactions.modify_category(category, new_category):
@@ -117,11 +119,10 @@ class Tracker:
             print(f"Category {category} modified to {new_category} successfully")
         else:
             print("Failed to modify category")
-    
 
     def show_transactions(self):
-        ''' Written by Gianna'''
-        '''returns a list of transations the user had inputted. '''
+        ''' Written by Gianna
+        returns a list of transations the user had inputted. '''
         transactions = self.transactions.get_transactions()
         if not transactions:
             print("No transactions found.")
@@ -129,10 +130,10 @@ class Tracker:
             print("Transactions:")
             for t in transactions:
                 print(t)
-    
+
     def add_transaction(self):
-        ''' Written by Gianna'''
-        ''' adds a transaction to the database'''
+        ''' Written by Gianna
+        adds a transaction to the database'''
         item = input("Enter item name: ")
         amount = input("Enter amount: ")
         category = input("Enter category: ")
@@ -142,15 +143,15 @@ class Tracker:
         print("Transaction added successfully.")
 
     def delete_transaction(self):
-        ''' Written by Samiya'''
-        ''' deletes a transaction from the database '''
+        ''' Written by Samiya
+        deletes a transaction from the database '''
         id = input("Enter transaction id: ")
         self.transactions.delete_transaction(id)
         print(f"Deleted transaction {id}.")
-    
+
     def summarize_by_date(self):
-        ''' Written by Cindy and Samiya'''
-        ''' will summarize the finance transactions by date'''
+        ''' Written by Cindy and Samiya
+        will summarize the finance transactions by date'''
         summary = self.transactions.summarize_by_date()
         if not summary:
             print("No transactions found.")
@@ -159,8 +160,8 @@ class Tracker:
                 print(row[0], "- $", row[1])
 
     def summarize_by_month(self):
-        ''' Written by Cindy and Gianna'''
-        ''' will summarize the finance transactions by month'''
+        ''' Written by Cindy and Gianna
+        will summarize the finance transactions by month'''
         summary = self.transactions.summarize_by_month()
         if not summary:
             print("No transactions found.")
@@ -169,8 +170,8 @@ class Tracker:
                 print(f"{month}: ${total:.2f}")
 
     def summarize_by_year(self):
-        ''' Written by Gianna'''
-        ''' will summarize the finance transactions by year'''
+        ''' Written by Gianna
+        will summarize the finance transactions by year'''
         summary = self.transactions.summarize_by_year()
         if not summary:
             print("No transactions found.")
@@ -179,8 +180,8 @@ class Tracker:
                 print(f"{year}: ${total:.2f}")
 
     def summarize_by_category(self):
-        ''' Written by Gianna'''
-        ''' will summarize the finance transactions by category'''
+        ''' Written by Gianna
+        will summarize the finance transactions by category'''
         summary = self.transactions.summarize_by_category()
         if not summary:
             print("No transactions found.")
@@ -189,8 +190,7 @@ class Tracker:
                 print(f"{month}: ${total:.2f}")
 
 if __name__ == "__main__":
-    ''' Written by Samiya'''
-    ''' will run the main method of the program'''
+    # Written by Samiya
+    # will run the main method of the program
     tracker = Tracker("finance.db")
     tracker.run()
-

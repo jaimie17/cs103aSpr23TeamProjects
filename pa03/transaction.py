6de3deb44b26a7e7f0aec7d1ae5de7acbf37e5cd
cindychi <123279185+cindychi@users.noreply.github.com>
2023-03-26 00:18:07 -0400

    Update transaction.py

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -16,12 +16,16 @@ import os
 
 class Transaction:
     def __init__(self, db_file: str) -> None:
-        ''' Written by Samiya'''
+        ''' Initializes the Transaction object and the various functions to perform ‘’’
+        Written by Samiya '''
+        
         self.conn = sqlite3.connect(db_file)
         self.create_table()
 
     def create_table(self) -> None:
-        ''' Written by Samiya'''
+        ''' Creates a table with sections: ID, item number, amount, category, date, and description
+        Written by Samiya'''
+        
         self.conn.execute('''CREATE TABLE IF NOT EXISTS transactions
                             (id INTEGER PRIMARY KEY,
                              item_number TEXT,
@@ -31,7 +35,9 @@ class Transaction:
                              description TEXT)''')
 
     def add_transaction(self, item_number: str, amount: float, category: str, date: str, description: str) -> None:
-        ''' Written by Gianna'''
+        ''' Adds transaction to the database by taking in the item number, amount, category, date, and description
+        Written by Gianna'''
+        
         self.conn.execute(
             "INSERT INTO transactions (item_number, amount, category, date, description) VALUES (?, ?, ?, ?, ?)",
             (item_number, amount, category, date, description)
@@ -39,7 +45,9 @@ class Transaction:
         self.conn.commit()
 
     def delete_transaction(self, id: int) -> None:
-        ''' Written by Gianna'''
+        ''' Deletes transactions 
+        Written by Gianna'''
+        
         # check if item number exists in the table
         cursor = self.conn.execute(f"SELECT item_number FROM transactions WHERE id = '{id}'")
         result = cursor.fetchone()
@@ -49,7 +57,9 @@ class Transaction:
         self.conn.commit()
 
     def add_category(self, category: str) -> bool:
-       ''' Written by Jaimie and Samiya'''
+       ''' Adds a category to the database 
+       Written by Jaimie and Samiya'''
+       
        if self.get_category_id(category):
             return False
        self.conn.execute(
@@ -58,7 +68,9 @@ class Transaction:
        return True
     
     def modify_category(self, category: str, new_category: str) -> bool:
-        ''' Written by Jaimie'''
+        ''' Modifies a category name
+        Written by Jaimie'''
+        
         if self.get_category_id(new_category):
             return False
         self.conn.execute(
@@ -67,7 +79,9 @@ class Transaction:
         return True
 
     def get_category_id(self, category: str) -> int:
-        ''' Written by Jaimie and Samiya'''
+        ''' Gets a category ID, given the type of category the user inputted 
+        Written by Jaimie and Samiya'''
+        
         cursor = self.conn.execute('''CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY, category TEXT)''')
         #ALTER TABLE categories ADD COLUMN name TEXT
         result = cursor.fetchone()
@@ -76,42 +90,56 @@ class Transaction:
         return result[0]
 
     def get_transactions(self) -> List[Tuple]:
-        ''' Written by Samiya and Gianna'''
+        ''' Gets the transactions from the database
+        Written by Samiya and Gianna '''
+        
         cursor = self.conn.execute("SELECT * FROM transactions")
         return cursor.fetchall()
 
     def get_categories(self) -> List[str]:
-        ''' Written by Jaimie'''
+        ''' Gets the categories 
+        Written by Jaimie'''
+        
         cursor = self.conn.execute(
             "SELECT DISTINCT category FROM transactions UNION SELECT category FROM categories")
         categories = cursor.fetchall()
         return [category[0] for category in categories]
 
     def summarize_by_date(self) -> List[Tuple]:
-        ''' Written by Samiya'''
+        ''' Summarizes the transactions by date
+        Written by Samiya'''
+        
         cursor = self.conn.execute(
             "SELECT date, SUM(amount) FROM transactions GROUP BY date")
         return cursor.fetchall()
 
     def summarize_by_month(self) -> List[Tuple]:
-        ''' Written by Cindy'''
+        ''' Summarizes the transactions by month 
+        Written by Cindy'''
+        
         cursor = self.conn.execute(
             "SELECT strftime('%Y-%m', date) as month, SUM(amount) FROM transactions GROUP BY month")
         return cursor.fetchall()
 
     def summarize_by_year(self) -> List[Tuple]:
-        ''' Written by Cindy'''
+        ''' Summarizes the transactions by year 
+        Written by Cindy'''
+        
         cursor = self.conn.execute(
             "SELECT strftime('%Y', date) as year, SUM(amount) FROM transactions GROUP BY year")
         return cursor.fetchall()
 
     def summarize_by_category(self) -> List[Tuple]:
-        ''' Written by Cindy'''
+        ''' Summarizes the transactions by category
+         Written by Cindy'''
+         
         cursor = self.conn.execute(
             "SELECT category, SUM(amount) FROM transactions GROUP BY category")
         return cursor.fetchall()
 
     def __del__(self) -> None:
-        ''' Written by Samiya'''
+        ''' Closes out the program
+         Written by Samiya'''
+            
         self.conn.close()
 

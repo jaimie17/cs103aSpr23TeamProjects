8f9d95bca715e8ada61c125af5ee4bc0c794eacd
jaimie17 <jaimielouie@brandeis.edu>
2023-04-28 19:14:00 -0400

    fixed

--- ca02/routes/gpt.js
+++ ca02/routes/gpt.js
@@ -1,98 +1,58 @@
-// Import required libraries
 const express = require("express");
-const path = require("path");
-const { GPT } = require("openai");
-require("dotenv").config();
+const app = express.Router();
+const path = require('path');
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-// Initialize the app and OpenAI's GPT API
-const app = express();
-const gptAPI = new GPT(process.env.APIKEY);
+isLoggedIn = (req,res,next) => {
+  if (res.locals.loggedIn) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
 
-// Set the views directory and view engine
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-// Define a route for the GPT demo page
-app.get("/gptdemo", (req, res) => {
-  console.log("processing /gptdemo route");
-  res.render("gptdemo");
+// Define a route for the summaries page
+app.get("/index/summaries", (req, res) => {
+  console.log("processing /summaries route");
+  res.render("summaries");
 });
 
-// Define a route to handle form submissions
-app.post("/gptdemo", async (req, res) => {
-  console.log("processing form submission");
+// Define a route to handle summaries form submissions
+app.post("/index/summaries", async (req, res) => {
+  console.log("processing summaries form submission");
 
-  // Get the prompt from the form data
-  const prompt = req.body.prompt;
+  // Get the text to summarize from the form data
+  const text = req.body.text;
 
-  // Send the prompt to OpenAI's GPT API to get a response
-  const response = await gptAPI.complete({
-    prompt,
-    maxTokens: 150,
-    n: 1,
-    stop: "\n",
-  });
-  // Define a route for the poem page
-  app.get("/poem", (req, res) => {
-    console.log("processing /poem route");
-    res.render("poem");
-  });
+  // Generate a summary of the text using the GPT API
+  const summary = await gptAPI.generateSummary(text);
 
-  // Define a route to handle poem form submissions
-  app.post("/poem", async (req, res) => {
-    console.log("processing poem form submission");
-
-    // Get the prompt from the form data
-    const prompt = req.body.prompt;
-
-    // Generate a poem based on the prompt using the GPT API
-    const poem = await gptAPI.generatePoem(prompt);
-
-    // Render the poem on the page
-    res.render("poem", { poem });
-  });
-
-  // Define a route for the summaries page
-  app.get("/summaries", (req, res) => {
-    console.log("processing /summaries route");
-    res.render("summaries");
-  });
-
-  // Define a route to handle summaries form submissions
-  app.post("/summaries", async (req, res) => {
-    console.log("processing summaries form submission");
-
-    // Get the text to summarize from the form data
-    const text = req.body.text;
-
-    // Generate a summary of the text using the GPT API
-    const summary = await gptAPI.generateSummary(text);
-
-    // Render the summary on the page
-    res.render("summaries", { summary });
-  });
-
-  // Define a route for the Spanish translator page
-  app.get("/spanishTranslator", (req, res) => {
-    console.log("processing /spanishTranslator route");
-    res.render("spanishTranslator");
-  });
-
-  // Define a route to handle Spanish translator form submissions
-  app.post("/spanishTranslator", async (req, res) => {
-    console.log("processing Spanish translator form submission");
-
-    // Get the text to translate from the form data
-    const text = req.body.text;
-
-    // Translate the text to Spanish using the GPT API
-    const translation = await gptAPI.translateToSpanish(text);
-
-    // Render the translated text on the page
-    res.render("spanishTranslator", { translation });
-  });
-
-  // Render the response on the page
-  res.render("gptdemo", { response: response.choices[0].text });
+  // Render the summary on the page
+  res.render("summaries", { summary });
 });
 
+// Define a route for the Spanish translator page
+app.get("/index/spanishTranslations", (req, res) => {
+  console.log("processing /spanishTranslator route");
+  res.render("spanishTranslations");
+});
+
+// Define a route to handle Spanish translator form submissions
+app.post("/index/spanishTranslations", async (req, res) => {
+  console.log("processing Spanish translator form submission");
+
+  // Get the text to translate from the form data
+  const text = req.body.text;
+
+  // Translate the text to Spanish using the GPT API
+  const translation = await gptAPI.translateToSpanish(text);
+
+  // Render the translated text on the page
+  res.render("spanishTranslations", { translation });
+});
+
+module.exports = app;
